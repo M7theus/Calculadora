c111589917dedca0edb660c52058972a9b32a065
M7theus <m7theus_carvalho@protonmail.com>
2022-06-30 20:04:23 -0400

    Update skeleton.py

--- code/skeleton.py
+++ code/skeleton.py
@@ -1,14 +1,19 @@
 import PySimpleGUI as sg
 
-sg.theme('LightGrey1')
+def create_window(theme):
+    
+
+
+sg.theme('LightGrey5')
 sg.set_options(font = 'Franklin 14', button_element_size = (6,3))
 
-layout = [[sg.Input(size = (49,40))],
-          [sg.Button('E', size = (20,0)),sg.Button('C',size = (20,0))],
-          [sg.Button('7',size = (7,0)),sg.Button('8',size = (7,0)), sg.Button('9', size = (7,0)),sg.Button('*',size = (7,0))],
-          [sg.Button('4',size = (7,0)),sg.Button('5',size = (7,0)), sg.Button('6', size = (7,0)),sg.Button('/',size = (7,0))],
-          [sg.Button('1',size = (7,0)),sg.Button('2',size = (7,0)), sg.Button('3', size = (7,0)),sg.Button('-',size = (7,0))],
-          [sg.Button('0', size = (20,0)),sg.Button('.', size = (7,0)),sg.Button('+', size = (7,0))]
+button_size = (6,3)
+layout = [[sg.Text('Output', font = 'Franklin 26', justification = 'center', expand_x = True, pad = (10,20))],
+          [sg.Button('E', expand_x= True),sg.Button('C',expand_x=True)],
+          [sg.Button('7',size = button_size),sg.Button('8',size = button_size), sg.Button('9', size = button_size),sg.Button('*',size = button_size)],
+          [sg.Button('4',size = button_size),sg.Button('5',size = button_size), sg.Button('6', size = button_size),sg.Button('/',size = button_size)],
+          [sg.Button('1',size = button_size),sg.Button('2',size = button_size), sg.Button('3', size = button_size),sg.Button('-',size = button_size)],
+          [sg.Button('0', expand_x= True),sg.Button('.', size = button_size),sg.Button('+', size = button_size)]
           ]
 
 window = sg.Window('Calculator',layout)
